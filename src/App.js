859ef5bdc298dc23a794b20b8cdb9999e32e61df
brhane_giday <brhane5giday@gmail.com>
2020-12-27 12:32:12 +0300

    Initial Setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./index.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="grid__container">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a href="/">React Shopping Cart</a>
       </header>
+      <main>product List</main>
+      <footer>All Rights are Reserved</footer>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA3J2xzhD8WpVtw1WqiL1TpxkZAACu1wxQ",
+  authDomain: "north-mart.firebaseapp.com",
+  databaseURL: "https://north-mart.firebaseio.com",
+  projectId: "north-mart",
+  storageBucket: "north-mart.appspot.com",
+  messagingSenderId: "746735157308",
+  appId: "1:746735157308:web:99153d43b620e3ec774339",
+  measurementId: "G-GKG4RNBME9"
+};
